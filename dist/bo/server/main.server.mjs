67c66f6af275b003c4518e74997fabe9237a7624
OrniceDev <ornihouss1@gmail.com>
2025-06-24 15:38:10 +0100

    fix(data)

--- dist/bo/server/main.server.mjs
+++ dist/bo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EZ4GYWUR.mjs";import"./chunk-36EZUWB6.mjs";import"./chunk-WGABW74A.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2AN2QV3X.mjs";import"./chunk-36EZUWB6.mjs";import"./chunk-WGABW74A.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
