1ca07d726673d81e1ca8a2f679b9c195d06b5b62
OrniceDev <ornihouss1@gmail.com>
2025-07-27 14:00:02 +0100

    feat(Test BO): MAJ des données

--- dist/bo/server/main.server.mjs
+++ dist/bo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-V55KMSJD.mjs";import"./chunk-X4ZGH6U3.mjs";import"./chunk-ELMPGZBV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2BIQXPFH.mjs";import"./chunk-X4ZGH6U3.mjs";import"./chunk-ELMPGZBV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/environments/environment.ots.ts
+++ src/environments/environment.ots.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_SCHEME: 'https',
-  API_DOMAIN: 'boservices-api.otech-services.bj/api',
-  API_FILE: 'boservices-api.otech-services.bj',
+  API_DOMAIN: 'boeservices-api.otech-services.bj/api',
+  API_FILE: 'boeservices-api.otech-services.bj',
   API_VERSION: 'v1'
 };
