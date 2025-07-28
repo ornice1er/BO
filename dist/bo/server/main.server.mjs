4685f4fb3b1cda1958151c4e46d319bf79e1e616
OrniceDev <ornihouss1@gmail.com>
2025-07-28 09:36:47 +0100

    fix(Maj) des données

--- dist/bo/server/main.server.mjs
+++ dist/bo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-37DKB24P.mjs";import"./chunk-4MKTXBEW.mjs";import"./chunk-KOZBRRBM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BCRQGFFU.mjs";import"./chunk-4MKTXBEW.mjs";import"./chunk-KOZBRRBM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
