19f4a48ffe1d88985589ec9140f9a6531b78ef24
Ornice <ornihouss1@gmail.com>
2026-01-14 20:21:18 +0100

    fix data

--- src/environments/environment.pprod.ts
+++ src/environments/environment.pprod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_SCHEME: 'https',
-  API_DOMAIN: 'boeservices-api.mtfp-ctd.bj/api',
-  API_FILE: 'boeservices-api.mtfp-ctd.bj',
+  API_DOMAIN: 'preprodmtfp.gouv.bj/pprod-boapi/api',
+  API_FILE: 'preprodmtfp.gouv.bj/pprod-boapi',
   API_VERSION: 'v1'
 };
