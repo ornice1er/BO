7bbb09ff8c574fbeb27e3d1b238b94e13035b74f
Ornice <ornihouss1@gmail.com>
2026-02-05 18:04:22 +0100

    fix data

---
+++ src/environments/environment.pprod-memp.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_SCHEME: 'https',
+  API_DOMAIN: 'backofficememp.gouv.bj/backend/api',
+  API_FILE: 'backofficememp.gouv.bj/backend/',
+  API_VERSION: 'v1'
+};
