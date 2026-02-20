7019a173d82dbe44cb298cb8e1f68c31c02248da
Ornice <ornihouss1@gmail.com>
2026-02-20 16:20:38 +0100

    fix data

--- dist/bo/server/main.server.mjs
+++ dist/bo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3KLHDYK7.mjs";import"./chunk-X4ZGH6U3.mjs";import"./chunk-ELMPGZBV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-CK4QVLFP.mjs";import"./chunk-X4ZGH6U3.mjs";import"./chunk-ELMPGZBV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
