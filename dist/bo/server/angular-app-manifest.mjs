05e6cdce1876922db0eaeb9ab62ecc80597789c3
Ornice <ornihouss1@gmail.com>
2026-03-18 16:01:50 +0100

    fix prod

--- dist/bo/server/angular-app-manifest.mjs
+++ dist/bo/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 67663, hash: '5a4a97e05f39487853049f2b0e4598fdd9aac2d79030c710b8d87a83f238a874', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 12446, hash: 'ea09e9e027c1faf858b8fb756a6d8702398f760354ba392d6cfdb24d6bcb09a6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 67663, hash: '8c2759db772a9668e4f1a0503f8727b7431d27feabf5fd2c4ce64dabf46bd8cb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 12446, hash: '842110f3a6a1cf7afe58fc39df6ea1e962708a2f492e973b4dd792b5ae1b91d4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-JUEFRJJP.css': {size: 495568, hash: 'Xf6OLBe9pYI', text: () => import('./assets-chunks/styles-JUEFRJJP_css.mjs').then(m => m.default)}
   },
 };
--- src/environments/environment.pprod-memp.ts
+++ src/environments/environment.pprod-memp.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_SCHEME: 'https',
-  API_DOMAIN: 'backofficememp.gouv.bj/backend/api',
-  API_FILE: 'backofficememp.gouv.bj/backend/',
+  API_DOMAIN: 'preprodmemp.gouv.bj/pprod-boapi/api',
+  API_FILE: 'preprodmemp.gouv.bj/pprod-boapi',
   API_VERSION: 'v1'
 };
