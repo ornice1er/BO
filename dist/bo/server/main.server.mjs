6b98136ada8775dc8da8d6e72b5794844173f20f
Ornice <ornihouss1@gmail.com>
2026-03-18 15:28:09 +0100

    correction frontend

--- dist/bo/server/main.server.mjs
+++ dist/bo/server/main.server.mjs
@@ -1,24 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  destroyAngularServerApp,
-  extractRoutesAndCreateRouteTree,
-  getOrCreateAngularServerApp,
-  main_server_default,
-  setAngularAppManifest
-} from "./chunk-FLMYNOVS.mjs";
-import {
-  resetCompiledComponents
-} from "./chunk-UGOUP4DL.mjs";
-import "./chunk-5WKMABBB.mjs";
-
-// angular:main-server-inject-manifest:angular:main-server-inject-manifest
-import manifest from "./angular-app-manifest.mjs";
-setAngularAppManifest(manifest);
-export {
-  main_server_default as default,
-  destroyAngularServerApp as \u0275destroyAngularServerApp,
-  extractRoutesAndCreateRouteTree as \u0275extractRoutesAndCreateRouteTree,
-  getOrCreateAngularServerApp as \u0275getOrCreateAngularServerApp,
-  resetCompiledComponents as \u0275resetCompiledComponents
-};
-//# sourceMappingURL=main.server.mjs.map
+import{f as e,g as t,i as o,j as p,k as m}from"./chunk-GR7AVPTR.mjs";import{Ac as r}from"./chunk-OJSSSWXJ.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
