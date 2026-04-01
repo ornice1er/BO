fdac5e8bfc61ff686ee2e2064eb67bafee82088a
Ornice <ornihouss1@gmail.com>
2026-04-01 12:47:53 +0100

    retrait dossier

---
+++ dist/bo/server/assets-chunks/index_server_html.mjs
@@ -0,0 +1,23 @@
+export default `<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Espace de traitement des e-services</title>
+    <!-- <base href="https://preprodmemp.gouv.bj/pprod-bo/"> -->
+    <base href="https://preprodmtfp.gouv.bj/pprod-bo/">
+
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@angular/material@15.0.0/prebuilt-themes/indigo-pink.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href="styles-JUEFRJJP.css"></head>
+<body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
+</script>
+  <app-root></app-root>
+<link rel="modulepreload" href="chunk-L233UBQG.js"><script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="scripts-SATGWWHT.js" defer=""></script><script src="main-Y36YRU53.js" type="module"></script></body>
+</html>
+`;
