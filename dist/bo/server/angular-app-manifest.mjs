65969aba83cc7f4d39fbf3c45815cba32e2709c7
Ornice <ornihouss1@gmail.com>
2026-04-07 19:21:49 +0100

    new prod

--- dist/bo/server/angular-app-manifest.mjs
+++ dist/bo/server/angular-app-manifest.mjs
@@ -6,16 +6,22 @@ export default {
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {
+  "node_modules/ngx-quill/fesm2022/ngx-quill-quill-CUw8Q_m0.mjs": [
+    {
+      "path": "chunk-XLZOUWF7.js",
+      "dynamicImport": false
+    }
+  ],
   "node_modules/@angular/animations/fesm2022/browser.mjs": [
     {
-      "path": "chunk-IPGCH4IW.js",
+      "path": "chunk-266HUTDW.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 56892, hash: '570cb4aa07c034b730668ab0c91cf3f09490f9ac67cae4e6275f92af07012bd3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1675, hash: 'a90156d05eed9f548a38be37ee4b6a27633425e3359ddc5ef7388c110c886379', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-JUEFRJJP.css': {size: 495568, hash: 'Xf6OLBe9pYI', text: () => import('./assets-chunks/styles-JUEFRJJP_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 56994, hash: '822921eee267f6daf854198025824cf4d94c9d46840758c00738f8b49238491c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1777, hash: '0d4ef522c7e2b11a5c4dc710313d69374b09555479886305dde117557a168928', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-SD3T7DTI.css': {size: 495728, hash: 'w8puJgMvze8', text: () => import('./assets-chunks/styles-SD3T7DTI_css.mjs').then(m => m.default)}
   },
 };
