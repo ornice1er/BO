88e67b73b4935febd3e1638839fb5e84dc7fa675
Ornice <ornihouss1@gmail.com>
2026-05-07 17:44:12 +0100

    MAJ des données

--- dist/bo/server/angular-app-manifest.mjs
+++ dist/bo/server/angular-app-manifest.mjs
@@ -40,8 +40,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 57060, hash: 'b654104189b557a9b69d0bac8ce86467e670a10b43781438a714171373506e6c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1790, hash: '02677ae0526da8031f30a6bd55db97006a75fa7064375f4e0ed841fefe4052b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 57060, hash: '54f805797853500b8d737f7395909cdb0599c0d4be5b1385b8474e88c71b983b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1790, hash: 'eaed38069e0db76d17f7ac2dc9ebb23d37285710cfdd94447638966f12d571b6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-F53JYRNN.css': {size: 521927, hash: 'ehD34iksRXA', text: () => import('./assets-chunks/styles-F53JYRNN_css.mjs').then(m => m.default)}
   },
 };
